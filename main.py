b6881cf7a717bb5c18dc156d3a2c6685f835adc8
carlos-decelera <carlos@decelera.com>
2026-02-19 15:55:26 +0100

    s

--- main.py
+++ main.py
@@ -457,6 +457,7 @@ else:
                 margin=dict(t=120, b=50, l=40, r=40),
                 xaxis={'categoryorder':'total descending'},
                 coloraxis_showscale=False,
+                cliponaxis=False,
                 paper_bgcolor='rgba(0,0,0,0)',
                 plot_bgcolor='rgba(0,0,0,0)'
             )
